5bc55edb39e3777eed85157fcfa34857f09fd7c7
Duygu Yigitsoy <duyguyigitsoy@gmail.com>
2020-04-27 11:40:51 -0400

    HTML, CSS and JS files for report generating

---
+++ __battletest__/__result__/index.js
@@ -0,0 +1,331 @@
+// const fs = require("fs")
+// const data;
+// fs.readFileSync("./_pet_:petID_PUT.json", (err, result) => {
+//     if(err) throw(err)
+//     data = result
+// })
+
+// const data = fs.readdirSync(directory);
+
+const data  = {
+    "/pet/:petID_PUT Base Scenario": {
+      "request": {
+        "body": {
+          "name": "string",
+          "petType": "string",
+          "favoriteFoods": [
+            "o0x83s1pwy",
+            "oj9dn1lp0l",
+            "8ziudenp2g",
+            "4c03q6de7t",
+            "u4p943dr3g"
+          ],
+          "family": {
+            "mom": "string",
+            "dad": "string",
+            "siblings": {
+              "sisters": [
+                "iipgeg2fis",
+                "dx5jgqz8xt",
+                "cizaja1o9m",
+                "xtnpivyc4k",
+                "tlx4pbng3x"
+              ],
+              "brothers": [
+                "hpw9z2h1ac",
+                "2rtfl1vrit",
+                "0jhzrpayei",
+                "bg3bibswm9",
+                "6bl8m9554g"
+              ]
+            }
+          }
+        },
+        "query": []
+      },
+      "response": {
+        "status": 404,
+        "body": {}
+      },
+      "timer": 84,
+      "passed": false
+    },
+    "/pet/:petID_PUT {\"path\":{\"petID\":\"2^53 - 1\"}}": {
+      "request": {
+        "body": {
+          "name": "string",
+          "petType": "string",
+          "favoriteFoods": [
+            "o0x83s1pwy",
+            "oj9dn1lp0l",
+            "8ziudenp2g",
+            "4c03q6de7t",
+            "u4p943dr3g"
+          ],
+          "family": {
+            "mom": "string",
+            "dad": "string",
+            "siblings": {
+              "sisters": [
+                "iipgeg2fis",
+                "dx5jgqz8xt",
+                "cizaja1o9m",
+                "xtnpivyc4k",
+                "tlx4pbng3x"
+              ],
+              "brothers": [
+                "hpw9z2h1ac",
+                "2rtfl1vrit",
+                "0jhzrpayei",
+                "bg3bibswm9",
+                "6bl8m9554g"
+              ]
+            }
+          }
+        },
+        "query": []
+      },
+      "response": {
+        "status": 404,
+        "body": {}
+      },
+      "timer": 9,
+      "passed": false
+    },
+    "/pet/:petID_PUT {\"path\":{\"petID\":\"-(2^53 - 1)\"}}": {
+      "request": {
+        "body": {
+          "name": "string",
+          "petType": "string",
+          "favoriteFoods": [
+            "o0x83s1pwy",
+            "oj9dn1lp0l",
+            "8ziudenp2g",
+            "4c03q6de7t",
+            "u4p943dr3g"
+          ],
+          "family": {
+            "mom": "string",
+            "dad": "string",
+            "siblings": {
+              "sisters": [
+                "iipgeg2fis",
+                "dx5jgqz8xt",
+                "cizaja1o9m",
+                "xtnpivyc4k",
+                "tlx4pbng3x"
+              ],
+              "brothers": [
+                "hpw9z2h1ac",
+                "2rtfl1vrit",
+                "0jhzrpayei",
+                "bg3bibswm9",
+                "6bl8m9554g"
+              ]
+            }
+          }
+        },
+        "query": []
+      },
+      "response": {
+        "status": 404,
+        "body": {}
+      },
+      "timer": 12,
+      "passed": false
+    },
+    "/pet/:petID_PUT {\"path\":{\"petID\":\"1.79e+308\"}}": {
+      "request": {
+        "body": {
+          "name": "string",
+          "petType": "string",
+          "favoriteFoods": [
+            "o0x83s1pwy",
+            "oj9dn1lp0l",
+            "8ziudenp2g",
+            "4c03q6de7t",
+            "u4p943dr3g"
+          ],
+          "family": {
+            "mom": "string",
+            "dad": "string",
+            "siblings": {
+              "sisters": [
+                "iipgeg2fis",
+                "dx5jgqz8xt",
+                "cizaja1o9m",
+                "xtnpivyc4k",
+                "tlx4pbng3x"
+              ],
+              "brothers": [
+                "hpw9z2h1ac",
+                "2rtfl1vrit",
+                "0jhzrpayei",
+                "bg3bibswm9",
+                "6bl8m9554g"
+              ]
+            }
+          }
+        },
+        "query": []
+      },
+      "response": {
+        "status": 404,
+        "body": {}
+      },
+      "timer": 11,
+      "passed": false
+    },
+    "/pet/:petID_PUT {\"path\":{\"petID\":\"positive infinity\"}}": {
+      "request": {
+        "body": {
+          "name": "string",
+          "petType": "string",
+          "favoriteFoods": [
+            "o0x83s1pwy",
+            "oj9dn1lp0l",
+            "8ziudenp2g",
+            "4c03q6de7t",
+            "u4p943dr3g"
+          ],
+          "family": {
+            "mom": "string",
+            "dad": "string",
+            "siblings": {
+              "sisters": [
+                "iipgeg2fis",
+                "dx5jgqz8xt",
+                "cizaja1o9m",
+                "xtnpivyc4k",
+                "tlx4pbng3x"
+              ],
+              "brothers": [
+                "hpw9z2h1ac",
+                "2rtfl1vrit",
+                "0jhzrpayei",
+                "bg3bibswm9",
+                "6bl8m9554g"
+              ]
+            }
+          }
+        },
+        "query": []
+      },
+      "response": {
+        "status": 404,
+        "body": {}
+      },
+      "timer": 9,
+      "passed": true,
+    },
+    "/pet/:petID_PUT {\"path\":{\"petID\":\"negative infinity\"}}": {
+      "request": {
+        "body": {
+          "name": "string",
+          "petType": "string",
+          "favoriteFoods": [
+            "o0x83s1pwy",
+            "oj9dn1lp0l",
+            "8ziudenp2g",
+            "4c03q6de7t",
+            "u4p943dr3g"
+          ],
+          "family": {
+            "mom": "string",
+            "dad": "string",
+            "siblings": {
+              "sisters": [
+                "iipgeg2fis",
+                "dx5jgqz8xt",
+                "cizaja1o9m",
+                "xtnpivyc4k",
+                "tlx4pbng3x"
+              ],
+              "brothers": [
+                "hpw9z2h1ac",
+                "2rtfl1vrit",
+                "0jhzrpayei",
+                "bg3bibswm9",
+                "6bl8m9554g"
+              ]
+            }
+          }
+        },
+        "query": []
+      },
+      "response": {
+        "status": 404,
+        "body": {}
+      },
+      "timer": 6,
+      "passed": false
+    },
+    }
+const passedBlock = document.querySelector('#passed')
+const failedTable = document.querySelector('#failed')
+const parser = new DOMParser();
+
+function changeShow(elementId) {
+    let el = document.getElementById(elementId)
+    if (el.style.display === 'none') {
+        // show element if it's currently hidden
+        el.style.display = ''
+    } else {
+        // hide if it's currently shown
+        el.style.display = 'none'
+    }
+}
+
+function testInfo() {
+    // `escape` is used to prevent JS from interpreting quotes in `eachTest` as the end of the onClick argument
+    for (let eachTest in data) {
+        if (data[eachTest]["passed"]) {
+            const trTemplate = `
+            <tr>
+                <td>
+                    <p><button onClick="changeShow('${escape(eachTest)}')">PASSED</button><span class="endpoint">${eachTest}</span></p>
+                    <div id="${escape(eachTest)}" class="results" style="display: none">
+                        <ul class="result_keys">
+                            <li>Timer</li>
+                        </ul>
+                        <ul class="result_values">
+                            <li>${data[eachTest]["timer"]}</li>
+                        </ul>
+                    </div>
+                </td>
+            </tr>
+            `
+            const resultHTML = parser.parseFromString(trTemplate, 'text/html')
+            passedBlock.appendChild(resultHTML.documentElement)
+        } 
+        else {
+            const trTemplate = `
+                <tr>
+                    <td>
+                        <p><button onClick="changeShow('${escape(eachTest)}')">FAILED</button><span class="endpoint">${eachTest}</span></p>
+                        <div id="${escape(eachTest)}" class="results" style="display: none">
+                            <ul class="result_keys">
+                                <li>Request</li>
+                                <li>Status</li>
+                                <li>Timer</li>
+                                <li>Body</li>
+                            </ul>
+                            <ul class="result_values">
+                                <li>QUERY</li>
+                                <li>${data[eachTest]["response"]["status"]}</li>
+                                <li>${data[eachTest]["timer"]}</li>
+                                <li>
+                                    <pre>${JSON.stringify(data[eachTest]["request"]["body"], null, '\t')}</pre>
+                                </li>
+                            </ul>
+                        </div>
+                    </td>
+                </tr>
+            `
+            const resultHTML = parser.parseFromString(trTemplate, `text/html`)
+            failedTable.appendChild(resultHTML.documentElement)
+        }
+    }
+}
+
+testInfo()
